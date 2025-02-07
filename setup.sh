9303ae9e3a9f53bf8861f459b9a588eccb572fe5
motii <108280115+motii8128@users.noreply.github.com>
2025-02-07 23:56:00 +0900

    Add docker setup

--- setup.sh
+++ setup.sh
@@ -68,3 +68,26 @@ sudo add-apt-repository ppa:kicad/kicad-8.0-releases
 sudo apt update
 
 sudo apt install kicad
+
+log_info "Dockerのインストールを開始します"
+
+sleep 1
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER && newgrp docker
+
+log_info "すべてのインストールが完了しました"
